9cfc6f29f5ea45778ea4490c733f383bfc700d7f
matiasfeliu92 <matumazparrote@gmail.com>
2024-05-14 16:22:13 -0300

    change the sql connection driver using psycopg2

--- config/sql_db.py
+++ config/sql_db.py
@@ -7,12 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 database_url = os.getenv('DATABASE_URL')
 
-# def create_sql_connection():
-#     engine = create_engine(database_url)
-#     print("-------------POSTGRESQL_URL----------------")
-#     print(engine.url)
-#     return engine
-
 def create_sql_connection():
     conn = psycopg2.connect(database_url)
     return conn
