f781eda8e734b54e9e74a70d4dadcf5241b1d44e
matiasfeliu92 <matumazparrote@gmail.com>
2024-05-16 16:23:33 -0300

    update mongo db connection

--- config/mongo_db.py
+++ config/mongo_db.py
@@ -1,9 +1,14 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ssl_support
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 mongo_uri = os.getenv('MONGO_URI')
-client = MongoClient(mongo_uri)
+client = MongoClient(mongo_uri, tlsAllowInvalidCertificates=True)
 
-mongo_conn = client.get_database("etL_practise")
+try:
+    mongo_conn = client.get_database("etL_practise")
+    print("Connection successful")
+    print(mongo_conn.list_collection_names())
+except Exception as e:
+    print(f"Connection failed: {e}")
